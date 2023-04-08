23688b4d55f571bb014d7c5fdcd5792669a79306
kshetritej <furiousdtej@gmail.com>
2023-04-08 14:52:46 +0545

    foreach statement that is not fine

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function divide(a, b) {
   return a / b;
 }
 
+const container = document.querySelector(".container");
+const calcDisplay = document.querySelector(".calc-screen");
+const clearBtn = document.querySelector(".clear-btn");
+const percentageBtn = document.querySelector(".percentage-btn");
+const buttons = document.querySelectorAll("#btn");
+
+clearBtn.addEventListener("click",()=>{
+
+})
+
+
+
+buttons.forEach(button => {
+  button.addEventListener("click",()=>{
+    input = button.textContent;
+    console.log(input); //returns clicked number in console
+    return input;
+  });
+});
