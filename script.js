40ddd2405cd7bebb62e13a7a2c17b3cfd5080dde
kshetritej <furiousdtej@gmail.com>
2023-04-08 21:02:28 +0545

    operate function finished

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-
-function add(a, b) {
-  return a + b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  return a / b;
+function add(a,b){
+  return (a+b);
 }
 
-const container = document.querySelector(".container");
-const calcDisplay = document.querySelector(".calc-screen");
-const clearBtn = document.querySelector(".clear-btn");
-const percentageBtn = document.querySelector(".percentage-btn");
-const buttons = document.querySelectorAll("#btn");
-
-clearBtn.addEventListener("click",()=>{
-  calcDisplay.textContent= "0";
-})
-
-
-
-function getInput(){
-  buttons.forEach(button => {
-    button.addEventListener("click",()=>{
-      input = button.textContent;
-      return input;
-    });
-  });
+function subtract(a,b){
+  return (a-b);
 }
 
-console.log(getInput());
+function multiply(a,b){
+  return (a*b);
+}
+
+function divide(a,b){
+  if(b === 0){
+    return "LOL";
+  }
+  else{
+    return (a/b);
+  }
+}
+
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const defaultScreenValue = 0;
+
+
+function operate(operator,firstNumber,secondNumber){
+  switch(operator){
+    case "+":
+      return add(firstNumber,secondNumber);
+    case "-":
+      return subtract(firstNumber,secondNumber);
+    case "*":
+      return multiply(firstNumber,secondNumber);
+    case "/":
+      return divide(firstNumber,secondNumber);
+    default:
+      return "HUH!"
+  }
+}
+// console.log(operate("+",3,3)); //
+
+function updateDisplay(){
+
+}
+
