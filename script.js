ad6f2969c08d34ae59b0d9c42cf3bd3eb2e0608a
kshetritej <furiousdtej@gmail.com>
2023-04-12 11:39:03 +0545

    works as intended no idea whats going on

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let displayValue = null;
+let result = "";
+
+// All Clear Button
+const clearButton = document.querySelector("#clear-btn");
+clearButton.addEventListener("click",()=>{
+  firstNumber = null;
+  secondNumber = null;
+  operator = null;
+  screen.textContent = "0";
+})
+
+
+// Create the functions that populate the display 
+//when you click the number buttons. 
+//You should be storing the ‘display value’ 
+//in a variable somewhere for use in the next step.
+const numberButtons = document.querySelectorAll(".btn");
+const screen = document.querySelector(".calc-screen");
+numberButtons.forEach(numberButton => {
+  numberButton.addEventListener("click", () => {
+    screenContent = screen.textContent;
+    if(screenContent == "0" || screenContent == 0){
+      screen.textContent = numberButton.textContent;
+      displayValue = screen.textContent;
+    }
+    else{
+      screen.textContent += numberButton.textContent;
+      displayValue = screen.textContent;
+    }
+    displayValue =screen.textContent;
+
+    if(!operator){
+      firstNumber = displayValue;
+      console.log(firstNumber);
+    }
+    if(operator && firstNumber){
+      secondNumber = displayValue;
+      console.log(secondNumber);
+    }
+    if(operator && firstNumber  && secondNumber){
+      result = operate(operator, Number(firstNumber),Number(secondNumber));
+      screen.textContent = result;
+
+      firstNumber = result;
+      operator = "";
+      secondNumber = "";
+    }
+  });
+});
+
+/*Make the calculator work! 
+You’ll need to store the first number 
+that is input into the calculator when a user presses an operator,
+ and also save which operation has been chosen and then operate() 
+ on them when the user presses the “=” key */
+ 
+ const operatorButtons = document.querySelectorAll("#operator");
+ operatorButtons.forEach(operatorButton =>{
+  operatorButton.addEventListener("click",()=>{
+    operator = operatorButton.textContent;
+    screen.textContent = "";
+
+  });
+ });
+
+ function calculate(){
+
+ }
+ //Equal Button
+ const equalButton = document.querySelector("#equal-btn");
+ equalButton.addEventListener("click",()=>{
+  calculate();
+ })
+
+//  Operation functions
 function addition(a, b) {
   return a + b;
 }
@@ -14,11 +93,6 @@ function division(a, b) {
   a === 0 ? "lol" : a / b;
 }
 
-let firstNumber = null;
-let operator = null;
-let secondNumber = null;
-
-
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
     case "+":
@@ -33,5 +107,3 @@ function operate(operator, firstNumber, secondNumber) {
       return "HUH!";
   }
 }
-
-console.log(operate("+",3,4))
